4cd60695149f387d32e8acbbdbb46a3ffec571d0
morganic-jpg <52764981+morganic-jpg@users.noreply.github.com>
2024-01-29 17:52:40 -0800

    Initial Commit
    Added Rust project with Rocket server in main.rs
    Added index.html for main static webpage
    Added favicon

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use rocket::fs::{FileServer, relative};
+
+#[macro_use] extern crate rocket;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![hello])
+    .mount("/", FileServer::from(relative!("static")))
+
+}
